fa09c3db4af438a82100de5392eabd18db687bb2
Tonworio <tmeindinyo@yahoo.com>
2025-08-03 21:25:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: 'https://exam-app-frontend-tau.vercel.app'}));
+app.use(cors({ origin: ['https://exam-app-frontend-tau.vercel.app', 'https://exam-app-frontend-1lf3d41e3-tonworios-projects.vercel.app'] })
+
 app.use(express.json());
 
 // Routes
@@ -32,6 +33,7 @@ const startServer = async () => {
 };
 
 startServer();
+
 
 
 
